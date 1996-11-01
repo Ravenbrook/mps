582307be48bb3e5be321d1c70a2b98947a1b49bb
Harlequin Hope DVCS <hope@harlequin.com>
1996-11-01 14:03:21 +0000

    branched from 1.13
    Copied from Perforce
     Change: 16227
     ServerID: perforce.ravenbrook.com

--- src/poolmfs.c
+++ src/poolmfs.c
@@ -1,6 +1,6 @@
 /* impl.c.poolmfs: MANUAL FIXED SMALL UNIT POOL
  *
- * $HopeName: MMsrc!poolmfs.c(trunk.12) $
+ * $HopeName: !poolmfs.c(trunk.13) $
  * Copyright (C) 1994,1995,1996 Harlequin Group, all rights reserved
  *
  * This is the implementation of the MFS pool class.
@@ -35,7 +35,7 @@
 #include "mpm.h"
 #include "poolmfs.h"
 
-SRCID(poolmfs, "$HopeName: MMsrc!poolmfs.c(trunk.12) $");
+SRCID(poolmfs, "$HopeName: !poolmfs.c(trunk.13) $");
 
 
 /*  == Round up ==
