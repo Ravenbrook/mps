f6d4ffa477577afea4b5426c5773f21c56fe3fab
Harlequin Hope DVCS <hope@harlequin.com>
1997-02-12 14:31:22 +0000

    branched from 1.1.7.1
    Copied from Perforce
     Change: 16611
     ServerID: perforce.ravenbrook.com

---
+++ src/ssan.c
@@ -0,0 +1,15 @@
+/* impl.c.ssan: ANSI STACK SCANNER
+ *
+ * $HopeName: !ssan.c(MMdevel_sw_eq.1) $
+ *
+ * This module provides zero functionality.  It exists to feed the
+ * linker (prevent linker errors).
+ */
+
+#include "mpm.h"
+#include "ss.h"
+
+Res StackScan(ScanState ss, Addr *stackBot)
+{
+  return ResUNIMPL;
+}
