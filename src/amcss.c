01d72e7b8fd25a1e9a4414eaa7ac7fc4e9c25a1f
Richard Brooksby <rb@ravenbrook.com>
1997-03-04 13:37:04 +0000

    Fixing bug in assertion
    Copied from Perforce
     Change: 17084
     ServerID: perforce.ravenbrook.com

--- src/amcss.c
+++ src/amcss.c
@@ -1,6 +1,6 @@
 /* impl.c.amcss: POOL CLASS AMC STRESS TEST
  *
- * $HopeName: !amcss.c(trunk.11) $
+ * $HopeName: MMsrc!amcss.c(MMdevel_action2.1) $
  * Copyright (C) 1996 Harlequin Group, all rights reserved
  */
 
@@ -88,8 +88,9 @@ static void *test(void *arg, size_t s)
       collections = c;
       printf("\nCollection %u, %lu objects.\n",
              c, (unsigned long)i);
-      for(r=0; r<NR_EXACT_ROOTS; ++r)
-        assert(dylan_check(exact_roots[r]));
+      for(r = 0; r < NR_EXACT_ROOTS; ++r)
+        assert(exact_roots[r] == OBJNULL ||
+               dylan_check(exact_roots[r]));
     }
 
     if(rnd() & 1)
