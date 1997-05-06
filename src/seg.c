8714fae00b05393abe6f5a13c5c20147e5e51a0c
Harlequin Hope DVCS <hope@harlequin.com>
1997-05-06 15:13:14 +0100

    branched from 1.3
    Copied from Perforce
     Change: 17963
     ServerID: perforce.ravenbrook.com

--- src/seg.c
+++ src/seg.c
@@ -1,6 +1,6 @@
 /* impl.c.seg: SEGMENTS
  *
- * $HopeName: MMsrc!seg.c(MMdevel_bufferscan.2) $
+ * $HopeName: !seg.c(trunk.3) $
  * Copyright (C) 1997 The Harlequin Group Limited.  All rights reserved.
  *
  * .design: The design for this module is design.mps.seg.
@@ -16,7 +16,7 @@
 
 #include "mpm.h"
 
-SRCID(seg, "$HopeName: MMsrc!seg.c(MMdevel_bufferscan.2) $");
+SRCID(seg, "$HopeName: !seg.c(trunk.3) $");
 
 
 /* SegCheck -- check the integrity of a segment */
