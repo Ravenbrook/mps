360e3481f53998e9301b4ac47c4e627b6a75a360
Richard Brooksby <rb@ravenbrook.com>
1997-06-18 14:53:25 +0100

    Adding bitfield widths
    Copied from Perforce
     Change: 18364
     ServerID: perforce.ravenbrook.com

--- src/mpmtypes.h
+++ src/mpmtypes.h
@@ -1,6 +1,6 @@
 /* impl.h.mpmtypes: MEMORY POOL MANAGER TYPES
  *
- * $HopeName: !mpmtypes.h(trunk.24) $
+ * $HopeName: MMsrc!mpmtypes.h(MMdevel_segabs.1) $
  * Copyright (C) 1997 The Harlequin Group Limited.  All rights reserved.
  *
  * .readership: MM developers.
@@ -132,6 +132,7 @@ typedef Res (*RootScanRegMethod)(ScanState ss, Thread thread, void *p,
 #define AccessSetEMPTY  ((AccessSet)0) /* design.mps.type.access-set */
 #define AccessREAD      ((AccessSet)(1<<0))
 #define AccessWRITE     ((AccessSet)(1<<1))
+#define AccessMAX	((Size)2)
 #define RingNONE        ((Ring)0)       /* design.mps.ring */
 #define TraceIdNONE     ((TraceId)-1)   /* design.mps.tracer */
 #define RefSetEMPTY     BS_EMPTY(RefSet)
@@ -154,8 +155,8 @@ typedef Res (*RootScanRegMethod)(ScanState ss, Thread thread, void *p,
                          AttrPM_NO_WRITE | AttrALLOC | AttrFREE | \
                          AttrBUF | AttrBUF_RESERVE | AttrBUF_ALLOC | \
                          AttrGC | AttrINCR_RB | AttrINCR_WB)
-#define SegPrefHigh     ((SegPrefKind) 0)
-#define SegPrefLow      ((SegPrefKind) 1)
+#define SegPrefHigh     ((SegPrefKind)0)
+#define SegPrefLow      ((SegPrefKind)1)
 #define SegPrefRefSet	((SegPrefKind)2)
 
 
