85ee172501abf201922d836c76da54eeb749a2cb
David Jones <drj@ravenbrook.com>
1997-07-03 14:20:22 +0100

    correcting RankSetUNIV
    Copied from Perforce
     Change: 18399
     ServerID: perforce.ravenbrook.com

--- src/mpmtypes.h
+++ src/mpmtypes.h
@@ -1,6 +1,6 @@
 /* impl.h.mpmtypes: MEMORY POOL MANAGER TYPES
  *
- * $HopeName: MMsrc!mpmtypes.h(MM_dylan_sunflower.1) $
+ * $HopeName: MMsrc!mpmtypes.h(MM_dylan_sunflower.2) $
  * Copyright (C) 1997 The Harlequin Group Limited.  All rights reserved.
  *
  * .readership: MM developers.
@@ -138,7 +138,7 @@ typedef Res (*RootScanRegMethod)(ScanState ss, Thread thread, void *p,
 #define RefSetUNIV      BS_UNIV(RefSet)
 #define TraceSetEMPTY	BS_EMPTY(TraceSet) /* design.mps.tracer */
 #define RankSetEMPTY	BS_EMPTY(RankSet)
-#define RankSetUNIV	BS_UNIV(RankSet)
+#define RankSetUNIV	((1uL<<RankMAX)-1)
 #define AttrFMT         ((Attr)(1<<0))  /* design.mps.type.attr */
 #define AttrSCAN        ((Attr)(1<<1))
 #define AttrPM_NO_READ  ((Attr)(1<<2))
