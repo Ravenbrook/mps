652c5e0c5cac3906d648e7bab7fd28c3b262ce05
Pekka Pirinen <pekka@harlequin.com>
1997-09-22 20:41:44 +0100

    Reduce arena size to make it run on deja-vu
    Copied from Perforce
     Change: 18796
     ServerID: perforce.ravenbrook.com

--- src/arenacv.c
+++ src/arenacv.c
@@ -1,6 +1,6 @@
 /* impl.c.arenacv: ARENA COVERAGE TEST
  *
- * $HopeName: !arenacv.c(trunk.5) $
+ * $HopeName: MMsrc!arenacv.c(MM_dylan_honeybee.1) $
  * Copyright (C) 1997 Harlequin Group, all rights reserved
  *
  * .readership: MPS developers
@@ -116,17 +116,20 @@ static void testit(ArenaClass class, ...)
 }
 
 
+#define TEST_ARENA_SIZE              ((Size)16<<20)
+
+
 int main(void)
 {
   void *block;
 
-  testit((ArenaClass)mps_arena_class_vm(), ARENA_SIZE);
+  testit((ArenaClass)mps_arena_class_vm(), TEST_ARENA_SIZE);
 
-  testit((ArenaClass)mps_arena_class_an(), ARENA_SIZE);
+  testit((ArenaClass)mps_arena_class_an(), TEST_ARENA_SIZE);
 
-  block = malloc(ARENA_SIZE);
+  block = malloc(TEST_ARENA_SIZE);
   die(block == NULL ? ResFAIL : ResOK, "malloc");
-  testit((ArenaClass)mps_arena_class_cl(), ARENA_SIZE, (Addr)block);
+  testit((ArenaClass)mps_arena_class_cl(), TEST_ARENA_SIZE, (Addr)block);
 
   fprintf(stderr, "Conclusion:  Failed to find any defects.\n");
   return 0;
