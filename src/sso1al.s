560d694918d9a9f10d51517f8a7a6cb517651b10
Gavin Matthews <grm@ravenbrook.com>
1997-09-26 17:48:57 +0100

    Use TraceScanArea
    Copied from Perforce
     Change: 18852
     ServerID: perforce.ravenbrook.com

--- src/sso1al.s
+++ src/sso1al.s
@@ -1,6 +1,6 @@
  # impl.s.sso1al: STACK SCANNING FOR DIGITAL UNIX / ALPHA
  #
- # $HopeName: !sso1al.s(trunk.1) $
+ # $HopeName: MMsrc!sso1al.s(MM_dylan_honeybee.1) $
  # Copyright (C) 1997 Harlequin Group, all rights reserved
  #
  # .readership: Any MPS developer that is prepared to read Alpha
@@ -10,7 +10,7 @@
 
 
 .globl	StackScan
-.globl	TraceScanAreaTagged
+.globl	TraceScanArea
 
 .ent	StackScan
 StackScan:
@@ -32,9 +32,9 @@ stq	$15,56($sp)
 bis	$31,$17,$18	# area to be scanned is from $sp to StackBot
 bis	$31,$sp,$17
 
-jsr	$26,TraceScanAreaTagged
+jsr	$26,TraceScanArea
 ldgp	$gp,0($26)
- # our result is TraceScanAreaTagged's result, so leave $0 untouched
+ # our result is TraceScanArea's result, so leave $0 untouched
 
 ldq	$26,0($sp)
 lda	$sp,+64($sp)
