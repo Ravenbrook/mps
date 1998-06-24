0e5205310e9c5b7b10cc4f322c60245006fcb0e5
P. Tucker Withington <ptw@harlequin.com>
1998-06-24 01:14:09 +0100

    Add fragmentation limit
    Copied from Perforce
     Change: 19704
     ServerID: perforce.ravenbrook.com

--- src/mpscmv2.h
+++ src/mpscmv2.h
@@ -1,6 +1,6 @@
 /* impl.h.mpscmv2: MEMORY POOL SYSTEM CLASS "MV2"
  *
- * $HopeName: MMsrc!mpscmv2.h(MMdevel_gavinm_splay.1) $
+ * $HopeName: MMsrc!mpscmv2.h(MMdevel_gavinm_splay.2) $
  * Copyright (C) 1998 Harlequin Group plc. All rights reserved.
  */
 
@@ -9,21 +9,27 @@
 
 #include "mps.h"
 
-/* The mv2 pool class has four extra parameters to mps_pool_create:
+/* The mv2 pool class has five extra parameters to mps_pool_create:
  *  mps_res_t mps_pool_create(mps_pool_t * pool, mps_arena_t arena,
  *                            mps_class_t mv2_class,
- *                            size_t min_size,
+ *                            size_t minimum_size,
  *                            size_t median_size,
  *                            size_t maximum_size,
- *                            mps_count_t reserve_depth);
- * min_, median_, and maximum_size are the mimimum, median, and
+ *                            mps_count_t reserve_depth
+ *                            mps_count_t fragmentation_limit);
+ * minimum_, median_, and maximum_size are the mimimum, median, and
  * maximum (typical) size of objects expected to be allocated in the
  * pool.  reserve_depth is a measure of the expected hysteresis of the
- * object population.
+ * object population.  fragmentation_limit is a percentage (between 0
+ * and 100):  if the free space managed by the pool exceeds the
+ * specified percentage, the pool will resort to a "first fit"
+ * allocation policy.
  */
-extern size_t mps_mv2_free_size(mps_pool_t mps_pool);
-extern size_t mps_mv2_size(mps_pool_t mps_pool);
 extern mps_class_t mps_class_mv2(void);
 
+/* The mv2 pool class supports two extensions to the pool protocol:
+   size and free_size. */
+extern size_t mps_mv2_free_size(mps_pool_t mps_pool);
+extern size_t mps_mv2_size(mps_pool_t mps_pool);
 
 #endif /* mpscmv2_h */
