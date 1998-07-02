2eafbf9712f097df479fc30d0f3355feaebfcd51
David Jones <drj@ravenbrook.com>
1998-07-02 10:33:45 +0100

    change.dylan.blackbird.1.160099, bug in AWLWhiten
    Copied from Perforce
     Change: 19726
     ServerID: perforce.ravenbrook.com

--- src/poolawl.c
+++ src/poolawl.c
@@ -1,6 +1,6 @@
 /* impl.c.poolawl: AUTOMATIC WEAK LINKED POOL CLASS
  *
- * $HopeName: MMsrc!poolawl.c(MM_dylan_blackbird.1) $
+ * $HopeName: MMsrc!poolawl.c(MM_dylan_blackbird.2) $
  * Copyright (C) 1998.  Harlequin Group plc.  All rights reserved.
  *
  * READERSHIP
@@ -46,7 +46,7 @@
 #include "mpm.h"
 
 
-SRCID(poolawl, "$HopeName: MMsrc!poolawl.c(MM_dylan_blackbird.1) $");
+SRCID(poolawl, "$HopeName: MMsrc!poolawl.c(MM_dylan_blackbird.2) $");
 
 
 #define AWLSig  ((Sig)0x519b7a37)       /* SIGPooLAWL */
@@ -619,12 +619,14 @@ static Res AWLWhiten(Pool pool, Trace trace, Seg seg)
     /* Check the buffer is black. */
     /* This really ought to change when we have a non-trivial */
     /* pre-flip phase. @@@@ ('coz then we'll be allocating white) */
-    AVER(BTIsSetRange(group->mark,
-                      awlIndexOfAddr(base, awl, BufferScanLimit(buffer)),
-		      awlIndexOfAddr(base, awl, BufferLimit(buffer))));
-    AVER(BTIsSetRange(group->scanned,
-                      awlIndexOfAddr(base, awl, BufferScanLimit(buffer)),
-		      awlIndexOfAddr(base, awl, BufferLimit(buffer))));
+    if(BufferScanLimit(buffer) != BufferLimit(buffer)) {
+      AVER(BTIsSetRange(group->mark,
+			awlIndexOfAddr(base, awl, BufferScanLimit(buffer)),
+			awlIndexOfAddr(base, awl, BufferLimit(buffer))));
+      AVER(BTIsSetRange(group->scanned,
+			awlIndexOfAddr(base, awl, BufferScanLimit(buffer)),
+			awlIndexOfAddr(base, awl, BufferLimit(buffer))));
+    }
     /* We didn't condemn the buffer, subtract it from the count. */
     /* @@@@ We could subtract all the free grains. */
     trace->condemned += SegSize(seg)
