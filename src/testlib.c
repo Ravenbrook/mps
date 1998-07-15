e4e63c31ea43455e956446752659d19ee3e5e566
Pekka Pirinen <pekka@harlequin.com>
1998-07-15 19:41:13 +0100

    Allow cranking collection frequencies all the way down
    Copied from Perforce
     Change: 19767
     ServerID: perforce.ravenbrook.com

--- src/testlib.c
+++ src/testlib.c
@@ -1,22 +1,23 @@
 /* impl.c.testlib: Test library
  *
- * $HopeName: MMsrc!testlib.c(MMdevel_ramp_alloc.1) $
+ * $HopeName: MMsrc!testlib.c(MMdevel_ramp_alloc.2) $
  *
- * Copyright (C) 1995, 1998 Harlequin Group, all rights reserved
+ * Copyright (C) 1995, 1998.  Harlequin Group plc.  All rights reserved.
  *
  * .purpose: A library of functions that may be of use to unit tests.
  */
 
-#include "mps.h"
 #include "testlib.h"
+#include "mps.h"
+#include "mpm.h"
+#include "mpstd.h"
 #ifdef MPS_OS_SU
 #include "ossu.h"
 #endif
-#include "mpm.h"
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 
 /* rnd -- a random number generator
@@ -52,19 +53,39 @@ void die(mps_res_t res, const char *s)
 
 /* adjust_collection_freq -- multiply all collection frequencies by
  *                           a given factor
+ *
+ * If frequencies are adjusted too low, they are corrected so that all are
+ * non-zero and larger than the ones for lower generations.
  */
 
 void adjust_collection_freq(double multiplier)
 {
   AMCGen0Frequency *= multiplier;
-  if(AMCGen0Frequency == 0) AMCGen0Frequency = 1;
+  if(AMCGen0Frequency == 0)
+    AMCGen0Frequency = 1;
   AMCGen1Frequency *= multiplier;
+  if(AMCGen1Frequency <= AMCGen0Frequency)
+    AMCGen1Frequency = AMCGen0Frequency + 1;
   AMCGen2Frequency *= multiplier;
+  if(AMCGen2Frequency <= AMCGen1Frequency)
+    AMCGen2Frequency = AMCGen1Frequency + 1;
   AMCGen2plusFrequencyMultiplier *= multiplier;
+  if(AMCGen2plusFrequencyMultiplier == 0)
+    AMCGen2plusFrequencyMultiplier = 1;
   AMCGen0RampmodeFrequency *= multiplier;
-  if(AMCGen0RampmodeFrequency == 0) AMCGen0RampmodeFrequency = 1;
+  if(AMCGen0RampmodeFrequency == 0)
+    AMCGen0RampmodeFrequency = 1;
   AMCGen1RampmodeFrequency *= multiplier;
+  if(AMCGen1RampmodeFrequency == AMCGen0RampmodeFrequency)
+    AMCGen1RampmodeFrequency = AMCGen0RampmodeFrequency + 1;
   AMCRampGenFrequency *= multiplier;
+  assert(AMCRampGenFollows == 1);
+  if(AMCRampGenFrequency <= AMCGen1RampmodeFrequency)
+    AMCRampGenFrequency = AMCGen1RampmodeFrequency + 1;
   AMCGen2RampmodeFrequency *= multiplier;
+  if(AMCGen2RampmodeFrequency <= AMCRampGenFrequency)
+    AMCGen2RampmodeFrequency = AMCRampGenFrequency * 2;
   AMCGen2plusRampmodeFrequencyMultiplier *= multiplier;
+  if(AMCGen2plusRampmodeFrequencyMultiplier == 0)
+    AMCGen2plusRampmodeFrequencyMultiplier = 1;
 }
