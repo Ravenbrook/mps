713b047be858c23e7eec5223453a5799e0c119a0
David Jones <drj@ravenbrook.com>
1999-01-15 10:51:22 +0000

    adding new interface
    Copied from Perforce
     Change: 20479
     ServerID: perforce.ravenbrook.com

--- src/mpm.h
+++ src/mpm.h
@@ -1,6 +1,6 @@
 /* impl.h.mpm: MEMORY POOL MANAGER DEFINITIONS
  *
- * $HopeName: !mpm.h(trunk.116) $
+ * $HopeName: MMsrc!mpm.h(MMdevel_drj_arena_hysteresis.1) $
  * Copyright (C) 1998.  Harlequin Group plc.  All rights reserved.
  */
 
@@ -666,6 +666,9 @@ extern Size ArenaCommitted(Arena arena);
 
 extern Size ArenaCommitLimit(Arena arena);
 extern Res ArenaSetCommitLimit(Arena arena, Size limit);
+extern Size ArenaSpareCommitLimit(Arena arena);
+extern void ArenaSetSpareCommitLimit(Arena arena, Size limit);
+extern void ArenaNoSpareCommitExceeded(Arena arena);
 
 extern double ArenaMutatorAllocSize(Arena arena);
 
