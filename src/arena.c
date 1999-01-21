9c980b13d9062e3ba595b10c0f04ac24fe049105
David Jones <drj@ravenbrook.com>
1999-01-21 18:16:04 +0000

    adding mps_arena_spare_committed interface
    Copied from Perforce
     Change: 20520
     ServerID: perforce.ravenbrook.com

--- src/arena.c
+++ src/arena.c
@@ -1,6 +1,6 @@
 /* impl.c.arena: ARENA IMPLEMENTATION
  *
- * $HopeName: MMsrc!arena.c(MM_epcore_brisling.1) $
+ * $HopeName: MMsrc!arena.c(MM_epcore_brisling.2) $
  * Copyright (C) 1998. Harlequin Group plc. All rights reserved.
  *
  * .readership: Any MPS developer
@@ -36,7 +36,7 @@
 #include "poolmrg.h"
 #include "mps.h"
 
-SRCID(arena, "$HopeName: MMsrc!arena.c(MM_epcore_brisling.1) $");
+SRCID(arena, "$HopeName: MMsrc!arena.c(MM_epcore_brisling.2) $");
 
 
 /* Forward declarations */
@@ -1383,6 +1383,12 @@ Size ArenaCommitted(Arena arena)
 {
   AVERT(Arena, arena);
   return (*arena->class->committed)(arena);
+}
+
+Size ArenaSpareCommitted(Arena arena)
+{
+  AVERT(Arena, arena);
+  return arena->spareCommitted;
 }
 
 Size ArenaSpareCommitLimit(Arena arena)
