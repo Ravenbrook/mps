6a2dcaef1349ece319f665dfb904195674f7ee76
Pekka Pirinen <pekka@harlequin.com>
1999-01-22 19:22:46 +0000

    unrelease
    Copied from Perforce
     Change: 20529
     ServerID: perforce.ravenbrook.com

--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
 /* impl.c.version: Version Inspection
  *
- * $HopeName: MMsrc!version.c(MM_epcore_brisling.3) $
+ * $HopeName: MMsrc!version.c(MM_epcore_brisling.4) $
  * Copyright (C) 1998.  Harlequin Group plc.  All rights reserved.
  *
  * PURPOSE
@@ -25,7 +25,7 @@
 /* .release: When making a new release, change the expansion of
  * MPS_RELEASE to be a string of the form "release.dylan.crow.2"
  * or whatever. */
-#define MPS_RELEASE "release.epcore.brisling.1"
+#define MPS_RELEASE "$HopeName$ *** DEVELOPMENT ONLY ***"
 
 /* Version String
  *
