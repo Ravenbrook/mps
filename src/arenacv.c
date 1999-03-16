a6de8eda615126f07a2206a1259c5051b0b7d190
Tony Mann <tony@harlequin.com>
1999-03-16 17:17:15 +0000

    change.mps.dylan.kinglet.160189: MPS representation of contiguous storage via fixed-size segments is insufficiently flexible.
    Copied from Perforce
     Change: 20624
     ServerID: perforce.ravenbrook.com

--- src/arenacv.c
+++ src/arenacv.c
@@ -1,15 +1,15 @@
 /* impl.c.arenacv: ARENA COVERAGE TEST
  *
- * $HopeName: !arenacv.c(trunk.10) $
+ * $HopeName: MMsrc!arenacv.c(MMdevel_tony_sunset.1) $
  * Copyright (C) 1997, 1998 Harlequin Group plc.  All rights reserved.
  *
  * .readership: MPS developers
  * .coverage: At the moment, we're only trying to cover the new code
  * (partial mapping of the page table and vm overflow).
- * .note.seg-size: If the page size is divisible by sizeof(SegStruct), many
+ * .note.tract-size: If the page size is divisible by sizeof(TractStruct), many
  * test cases end up being essentially identical -- there just aren't that
  * many different cases then.
- * .improve.gap-below: Could test different-sized gaps below the segment
+ * .improve.gap-below: Could test different-sized gaps below the tract
  * being allocated; this requires using two adjacent zones.
  */
 
@@ -27,15 +27,15 @@
 #endif
 
 
-#define segsSIZE 500
+#define tractsSIZE 500
 
 
 static void testPageTable(ArenaClass class, ...)
 {
   Arena arena; Pool pool;
-  Seg offsetSeg, gapSeg, newSeg, topSeg;
+  Addr offsetBase, gapBase, newBase, topBase;
   Size pageSize;
-  Count segsPerPage, offset, gap, new;
+  Count tractsPerPage, offset, gap, new;
   int i;
   SegPrefStruct pref = *SegPrefDefault();
   RefSet refSet = (RefSet)2;
@@ -50,66 +50,75 @@ static void testPageTable(ArenaClass class, ...)
       "PoolCreate");
 
   pageSize = ArenaAlign(arena);
-  segsPerPage = pageSize / sizeof(SegStruct);
-  printf("%ld segments per page in the page table.\n", (long)segsPerPage);
+  tractsPerPage = pageSize / sizeof(TractStruct);
+  printf("%ld tracts per page in the page table.\n", (long)tractsPerPage);
 
   /* Testing the behaviour with various sizes of gaps in the page table. */
 
   /* Assume the allocation strategy is first-fit.  The idea of the tests is */
-  /* to allocate a range of pages, then deallocate a gap in the middle, */
-  /* then allocate a new segment that fits in the gap with various amounts */
+  /* to allocate a region of memory, then deallocate a gap in the middle, */
+  /* then allocate a new region that fits in the gap with various amounts */
   /* left over.  Like this: */
-  /* |-offsetSeg-||----gapSeg----||-topSeg-| */
-  /* |-offsetSeg-||-newSeg-|      |-topSeg-| */
-  /* This is done with three different sizes of offsetSeg, in two different */
-  /* zones to ensure that all page boundary cases are tested. */
+  /* |-offsetRegion-||----gapRegion----||-topRegion-| */
+  /* |-offsetRegion-||-newRegion-|      |-topRegion-| */
+  /* This is done with three different sizes of offsetRegion, in two */
+  /*  different zones to ensure that all page boundary cases are tested. */
   for(i = 0; i < 2; ++i) { /* zone loop */
-    for(offset = 0; offset <= 2*segsPerPage; offset += segsPerPage) {
+    for(offset = 0; offset <= 2*tractsPerPage; offset += tractsPerPage) {
       if(offset != 0)
-        die(SegAlloc(&offsetSeg, &pref, offset * pageSize, pool,
-                     /* withReservoirPermit */ FALSE),
-            "offsetSeg");
-      for(gap = segsPerPage+1; gap <= 3 * (segsPerPage+1);
-          gap += (segsPerPage+1)) {
-        die(SegAlloc(&gapSeg, &pref, gap * pageSize, pool,
-                     /* withReservoirPermit */ FALSE),
-            "gapSeg");
-        die(SegAlloc(&topSeg, &pref, pageSize, pool,
-                     /* withReservoirPermit */ FALSE),
-            "topSeg");
-        SegFree(gapSeg);
-        for(new = 1; new <= gap; new += segsPerPage) {
-          Seg seg;
-
-          die(SegAlloc(&newSeg, &pref, new * pageSize, pool,
+        die(ArenaAlloc(&offsetBase, &pref, offset * pageSize, pool,
                        /* withReservoirPermit */ FALSE),
-              "newSeg");
+            "offsetRegion");
+      for(gap = tractsPerPage+1; gap <= 3 * (tractsPerPage+1);
+          gap += (tractsPerPage+1)) {
+        die(ArenaAlloc(&gapBase, &pref, gap * pageSize, pool,
+                       /* withReservoirPermit */ FALSE),
+            "gapRegion");
+        die(ArenaAlloc(&topBase, &pref, pageSize, pool,
+                       /* withReservoirPermit */ FALSE),
+            "topRegion");
+        ArenaFree(gapBase, gap * pageSize, pool);
+        for(new = 1; new <= gap; new += tractsPerPage) {
+          Tract tract;
+          Count tractNum, expected;
+          Res enoughTracts;
 
-          /* Test segment iterators */
-          die(SegFirst(&seg, arena) ? ResOK : ResFAIL, "first");
-          die(SegNext(&seg, arena, SegBase(seg)) ? ResOK : ResFAIL,
-              "second");
-          die(SegNext(&seg, arena, SegBase(seg)) ? ResOK : ResFAIL,
-              "third");
-          /* There are at least three segments */
-          SegNext(&seg, arena, SegBase(seg));
+          die(ArenaAlloc(&newBase, &pref, new * pageSize, pool,
+                         /* withReservoirPermit */ FALSE),
+              "newRegion");
 
-          SegFree(newSeg);
+          /* Test tract iterators */
+          die(TractFirst(&tract, arena) ? ResOK : ResFAIL, "first");
+          tractNum = 1;
+          while (TractNext(&tract, arena, TractBase(tract)))
+            tractNum++;
+
+          /* There are at least offset+new+top tracts */
+          expected = offset + new + 1;
+          if (tractNum >= expected) 
+            enoughTracts = ResOK;
+          else
+            enoughTracts = ResFAIL;
+
+          die(enoughTracts, "Not enough tracts");
+
+          ArenaFree(newBase, new * pageSize, pool);
         }
 
-      SegFree(topSeg);
+      ArenaFree(topBase, pageSize, pool);
       }
       if(offset != 0) {
-	/* Test size functions */
-	Addr base, limit;
-	Size size;
+	/* Test TractOfAddr */
+        Tract tract;
+	Addr base;
+        Bool found;
 
-	base = SegBase(offsetSeg);
-	limit = SegLimit(offsetSeg);
-	size = SegSize(offsetSeg);
-	die(size == AddrOffset(base, limit) ? ResOK : ResFAIL, "size");
+        found = TractOfAddr(&tract, arena, offsetBase);
+        die(found ? ResOK : ResFAIL, "TractOfAddr");
+	base = TractBase(tract);
+	die(base == offsetBase ? ResOK : ResFAIL, "base");
 
-	SegFree(offsetSeg);
+	ArenaFree(offsetBase, offset * pageSize, pool);
       }
     }
     SegPrefExpress(&pref, SegPrefRefSet, &refSet);
@@ -155,7 +164,7 @@ static void testSize(Size size)
 }
 
 
-#define TEST_ARENA_SIZE              ((Size)16<<20)
+#define TEST_ARENA_SIZE              ((Size)16<<22)
 
 
 int main(void)
