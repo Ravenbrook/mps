4e53763a9ea0bec07060256765e5b405fa356ed6
Pekka Pirinen <pekka@harlequin.com>
1999-03-23 18:25:21 +0000

    Update version to development
    Copied from Perforce
     Change: 20694
     ServerID: perforce.ravenbrook.com

--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
 /* impl.c.version: Version Inspection
  *
- * $HopeName: MMsrc!version.c(MM_epcore_brisling.11) $
+ * $HopeName: MMsrc!version.c(MM_epcore_brisling.12) $
  * Copyright (C) 1998.  Harlequin Group plc.  All rights reserved.
  *
  * PURPOSE
@@ -25,7 +25,7 @@
 /* .release: When making a new release, change the expansion of
  * MPS_RELEASE to be a string of the form "release.dylan.crow.2"
  * or whatever. */
-#define MPS_RELEASE "release.epcore.brisling.4 [$HopeName: MMsrc!version.c(MM_epcore_brisling.11) $]"
+#define MPS_RELEASE "release.epcore.brisling.4+ [$HopeName: MMsrc!version.c(MM_epcore_brisling.12) $] *** DEVELOPMENT ONLY ***"
 
 /* Version String
  *
