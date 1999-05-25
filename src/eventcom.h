9baecb405f3b4e14b472b6f70f2d179aaa35e407
Pekka Pirinen <pekka@harlequin.com>
1999-05-25 16:58:30 +0100

    New event types
    Copied from Perforce
     Change: 20839
     ServerID: perforce.ravenbrook.com

--- src/eventcom.h
+++ src/eventcom.h
@@ -1,7 +1,7 @@
 /* impl.h.eventcom -- Event Logging Common Definitions
  *
  * Copyright (C) 1997, 1998, 1999 Harlequin Group plc.  All rights reserved.
- * $HopeName: !eventcom.h(trunk.17) $
+ * $HopeName: MMsrc!eventcom.h(MMdevel_alloc_replay.1) $
  *
  * .readership: MPS developers
  * .sources: mps.design.telemetry
@@ -31,9 +31,8 @@ typedef struct {
 typedef EventStringStruct *EventString;
 
 
-/* @@@@ Put these in eventdef? */
 #define EventNameMAX ((size_t)19)
-#define EventCodeMAX ((EventCode)0x0041)
+#define EventCodeMAX ((EventCode)0x0064)
 
 
 /* eventgen.h is just the automatically generated part of this file */
@@ -55,7 +54,9 @@ typedef EventStringStruct *EventString;
 #define EventPoolFinish     ((EventType)0xEFB07F14) /* POoL FINish */
 #define EventPoolAlloc      ((EventType)0xEFB07A77) /* POoL ALLoc */
 #define EventPoolFree       ((EventType)0xEFB07F6E) /* POoL FREe */
-#define EventArenaCreate    ((EventType)0xEFA64C6E) /* AReNa CREate */
+#define EventArenaCreateVM  ((EventType)0xEFA64CF3) /* AReNa Create VM */
+#define EventArenaCreateVMNZ ((EventType)0xEFA64CF2) /* AReNa Create VmnZ */
+#define EventArenaCreateCL  ((EventType)0xEFA64CC7) /* AReNa Create CL */
 #define EventArenaDestroy   ((EventType)0xEFA64DE5) /* AReNa DEStroy */
 #define EventSegAlloc       ((EventType)0xEF5E9A77) /* SEG ALLoc */
 #define EventSegFree	    ((EventType)0xEF5E9F6E) /* SEG FREe */
@@ -64,7 +65,6 @@ typedef EventStringStruct *EventString;
 #define EventAMCInit        ((EventType)0xEFA3C141) /* AMC INIt */
 #define EventAMCFinish      ((EventType)0xEFA3CF14) /* AMC FINish */
 #define EventAMCTraceBegin  ((EventType)0xEFA3C26B) /* AMC TRace Begin */
-#define EventAMCCondemn     ((EventType)0xEFA3CC04) /* AMC CONdemn */
 #define EventAMCScanBegin   ((EventType)0xEFA3C5CB) /* AMC SCan Begin */
 #define EventAMCScanEnd     ((EventType)0xEFA3C5CE) /* AMC SCan End */
 #define EventAMCFix         ((EventType)0xEFA3CF18) /* AMC FIX */
@@ -104,6 +104,9 @@ typedef EventStringStruct *EventString;
 #define EventBufferReserve  ((EventType)0xEFB0FF6E) /* BUFFer REserve */
 #define EventBufferCommit   ((EventType)0xEFB0FFC0) /* BUFFer COmmit */
 #define EventBufferInit     ((EventType)0xEFB0FF14) /* BUFFer INit */
+#define EventBufferInitEPVM ((EventType)0xEFB0F1EF) /* BUFfer Init EpVm */
+#define EventBufferInitAMS  ((EventType)0xEFB0F1A5) /* BUFfer Init AmS */
+#define EventBufferInitAWL  ((EventType)0xEFB0F1A3) /* BUFfer Init AWl */
 #define EventBufferFinish   ((EventType)0xEFB0FFF1) /* BUFFer FInish */
 #define EventMV2Finish      ((EventType)0xEF3F2F14) /* MV2 FINish */
 #define EventBufferFill     ((EventType)0xEFB0FFF7) /* BUFFer FilL */
@@ -117,6 +120,23 @@ typedef EventStringStruct *EventString;
 #define EventTraceStatFix   ((EventType)0xEF26A5F8) /* TRAce Stat FiX */
 #define EventTraceStatReclaim ((EventType)0xEF26A56E) /* TRAce Stat REclaim */
 #define EventArenaWriteFaults ((EventType)0xEFA6436F) /* AReNa WRite Faults */
+#define EventPoolInitMV     ((EventType)0xEFB0713F) /* POoL Init MV */
+#define EventPoolInitMVFF   ((EventType)0xEFB071FF) /* POoL Init mvFF */
+#define EventPoolInitMFS    ((EventType)0xEFB07135) /* POoL Init MfS */
+#define EventPoolInitEPVM   ((EventType)0xEFB071EF) /* POoL Init EpVm */
+#define EventPoolInitEPDL   ((EventType)0xEFB071E7) /* POoL Init EpdL */
+#define EventPoolInitAMS    ((EventType)0xEFB071A5) /* POoL Init AmS */
+#define EventPoolInitAMC    ((EventType)0xEFB071AC) /* POoL Init AmC */
+#define EventPoolInitAMCZ   ((EventType)0xEFB071A2) /* POoL Init AmcZ */
+#define EventPoolInitAWL    ((EventType)0xEFB071A3) /* POoL Init AWl */
+#define EventPoolInitLO     ((EventType)0xEFB07170) /* POoL Init LO */
+#define EventPoolInitSNC    ((EventType)0xEFB07154) /* POoL Init SNc */
+#define EventPoolInitMV2    ((EventType)0xEFB07132) /* POoL Init Mv2 */
+#define EventPoolPush       ((EventType)0xEFB07B58) /* POoL PuSH */
+#define EventPoolPop        ((EventType)0xEFB07B0B) /* POoL POP */
+#define EventReservoirLimitSet ((EventType)0xEF6E5713) /* REServoir LIMit set */
+#define EventCommitLimitSet ((EventType)0xEFC03713) /* COMmit LIMit set */
+#define EventSpareCommitLimitSet ((EventType)0xEF5BC713) /* SPare Commit LIMit set */
 
 
 #endif /* eventcom_h */
