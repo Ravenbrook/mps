c1f9d73c8f05f308dc85cfd2d0dcde48093fb16f
Pekka Pirinen <pekka@harlequin.com>
2000-06-21 16:12:29 +0100

    remove release name
    Copied from Perforce
     Change: 21293
     ServerID: perforce.ravenbrook.com

--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
 /* impl.c.version: Version Inspection
  *
- * $HopeName: MMsrc!version.c(MM_epcore_brisling.15) $
+ * $HopeName: MMsrc!version.c(MM_epcore_brisling.16) $
  * Copyright (C) 1998 Harlequin Limited.  All rights reserved.
  *
  * PURPOSE
@@ -8,14 +8,10 @@
  * .purpose: The purpose of this module is to provide a means by which
  * the version of the MM library being used can be determined.
  *
- * READERSHIP
- *
- * .readership: Any MPS developers.
- *
  * DESIGN
  *
  * .design: See design.mps.version-library, but to let you in on a
- * secret it works by declaring a string with all the necessary info
+ * secret, it works by declaring a string with all the necessary info
  * in.
  */
 
@@ -25,7 +21,7 @@
 /* .release: When making a new release, change the expansion of
  * MPS_RELEASE to be a string of the form "release.dylan.crow.2"
  * or whatever. */
-#define MPS_RELEASE "release.epcore.brisling.7"
+#define MPS_RELEASE "release.epcore.brisling.7+ $HopeName: MMsrc!version.c(MMdevel_configura.1) $ *** DEVELOPMENT ONLY ***"
 
 /* Version String
  *
