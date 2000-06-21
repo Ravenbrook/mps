1a4a535d87c993c5d3fe03ce2c1060ec2266397a
Pekka Pirinen <pekka@harlequin.com>
2000-06-21 13:26:40 +0100

    record release.epcore.brisling.7
    Copied from Perforce
     Change: 21292
     ServerID: perforce.ravenbrook.com

--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
 /* impl.c.version: Version Inspection
  *
- * $HopeName: MMsrc!version.c(MM_epcore_brisling.14) $
+ * $HopeName: MMsrc!version.c(MM_epcore_brisling.15) $
  * Copyright (C) 1998 Harlequin Limited.  All rights reserved.
  *
  * PURPOSE
@@ -25,7 +25,7 @@
 /* .release: When making a new release, change the expansion of
  * MPS_RELEASE to be a string of the form "release.dylan.crow.2"
  * or whatever. */
-#define MPS_RELEASE "release.epcore.brisling.6+ [$HopeName: MMsrc!version.c(MM_epcore_brisling.14) $] *** DEVELOPMENT ONLY ***"
+#define MPS_RELEASE "release.epcore.brisling.7"
 
 /* Version String
  *
