bdb9de83bff3a5d0500c743c4cf9eda59b7149fd
Pekka Pirinen <pekka@harlequin.com>
2000-08-18 14:00:20 +0100

    Add BootCheck
    Copied from Perforce
     Change: 21435
     ServerID: perforce.ravenbrook.com

--- src/boot.h
+++ src/boot.h
@@ -1,6 +1,6 @@
 /* impl.h.boot: BOOTSTRAP ALLOCATOR INTERFACE
  *
- * $HopeName$
+ * $HopeName: MMsrc!boot.h(MMdevel_pekka_locus.1) $
  * Copyright (C) 2000 Harlequin Limited.  All rights reserved.
  *
  * .overview:  A protocol for allocating memory from a given block.
@@ -22,14 +22,13 @@ typedef struct BootBlockStruct
   void *limit;
 } BootBlockStruct;
 
-typedef BootBlockStruct *BootBlock;
-
 
 extern Res BootBlockInit(BootBlockStruct *boot, void *base, void *limit);
 extern void BootBlockFinish(BootBlock boot);
 extern Res BootAlloc(void **pReturn, BootBlock boot, size_t size,
                      size_t align);
 extern size_t BootAllocated(BootBlock boot);
+extern Bool BootBlockCheck(BootBlock boot);
 
 
 #endif /* boot_h */
