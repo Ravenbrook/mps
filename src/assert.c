28660731013f8bcc98c563eb958a715b80599e05
Pekka Pirinen <pekka@harlequin.com>
2000-09-12 19:13:29 +0100

    Suppress warning
    Copied from Perforce
     Change: 21495
     ServerID: perforce.ravenbrook.com

--- src/assert.c
+++ src/assert.c
@@ -1,19 +1,16 @@
 /* impl.c.assert: ASSERTION IMPLEMENTATION
  *
- * $HopeName: !assert.c(trunk.10) $
+ * $HopeName: MMsrc!assert.c(MM_dylan_kinglet.1) $
+ * Copyright (C) 1999 Harlequin Limited.  All rights reserved.
  *
  * This source provides the AssertFail function which is
  * invoked by the assertion macros (see impl.h.assert).
  * It also provides for user-installed assertion failure handlers.
- *
- *
- * NOTES
- *
- * .note.assert-buffer: The Assertion Buffer is not used.  In the future
- * we probably ought to use it. (see .buffer.* below)
  */
 
 #include "mpm.h"
+
+SRCID(assert, "$HopeName$");
 
 
 /* CheckLevel -- Control check level 
@@ -24,19 +21,6 @@
 
 Word CheckLevel = CheckSHALLOW;
 
-
-/* Assertion buffer
- *
- * .buffer.purpose: the assertion buffer serves two purposes:
- * .buffer.purpose.modify: assertion strings are picked apart and
- * modified before being passed to assertion handlers, this
- * buffer is used to do that.
- * .buffer.purpose.store: Debuggers, post-mortem dumps, etc, can retrieve the
- * assertion text even if for some reason printing the text out failed.
- * [note this is not used, see .note.buffer]
- */
-
-char AssertBuffer[ASSERT_BUFFER_SIZE];
 
 static void AssertLib(const char *cond, const char *id,
                       const char *file, unsigned line)
@@ -55,12 +39,15 @@ static void AssertLib(const char *cond, const char *id,
   mps_lib_abort();
 }
 
-static AssertHandler handler = AssertLib;
+
+static AssertHandler handler = &AssertLib;
+
 
 AssertHandler AssertDefault(void)
 {
-  return AssertLib;
+  return (AssertHandler)&AssertLib;
 }
+
 
 AssertHandler AssertInstall(AssertHandler new)
 {
