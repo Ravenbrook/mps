b914e6c4234ecabef8b82e2f23e6b6f6e995bc6e
Pekka Pirinen <pekka@harlequin.com>
2000-09-14 18:17:49 +0100

    Add copyright notice
    Copied from Perforce
     Change: 21510
     ServerID: perforce.ravenbrook.com

--- src/ssan.c
+++ src/ssan.c
@@ -1,13 +1,18 @@
 /* impl.c.ssan: ANSI STACK SCANNER
  *
- * $HopeName: !ssan.c(trunk.2) $
+ * $HopeName: MMsrc!ssan.c(MM_dylan_kinglet.1) $
+ * Copyright (C) 1996 Harlequin Limited.  All rights reserved.
  *
  * This module provides zero functionality.  It exists to feed the
  * linker (prevent linker errors).
  */
 
-#include "mpm.h"
+#include "mpmtypes.h"
+#include "misc.h"
 #include "ss.h"
+
+
+SRCID(ssan, "$HopeName$");
 
 
 Res StackScan(ScanState ss, Addr *stackBot)
