f92a088f8bb848883cd415928c30ee3020401f88
Richard Kistruck <rhsk@ravenbrook.com>
2008-07-28 19:00:49 +0100

    MPS br/awldiag: example/gc/gc2 -- how do I show current memory usage?
    Copied from Perforce
     Change: 165525
     ServerID: perforce.ravenbrook.com

--- example/gc/gc2.c
+++ example/gc/gc2.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>  /* for malloc */
 #include <stdio.h>  /* for printf */
 
@@ -8,8 +7,7 @@
 
 mps_res_t res;
 
-
-static void outres(char *text)
+static void checkres(char *text)
 {
   printf("%s -- res: %d\n", text, (int)res);
   if(res != MPS_RES_OK) {
@@ -31,46 +29,104 @@ static void error(char *text)
 
 /* XF -- an example format
  *
+ * Our format must allow:
+ *   - our objects (aka "client objects");
+ *   - padding objects (for MPS use);
+ *   - forwarding objects (for MPS use).
  */
 
 enum {
-  xf_type_atom = 'a',
-  xf_type_apad = 'p',
-  xf_type_vect = 'V',
-  xf_type_vpad = 'P',
-  xf_type_LAST = 100000  /* not a type */
+  xf_typecode_atom = 'a',
+  xf_typecode_vect = 'v',
+  xf_typecode_pad =  'p',
+  xf_typecode_fwdShort = 's',
+  xf_typecode_fwdLong  = 'l'
 };
 
-enum {
-  xf_value_atom = 'a',
-  xf_value_apad = ('a'<<24) + ('p'<<16) + ('a'<<8) + 'd',
-  xf_value_vect = 'V',
-  xf_value_vpad = ('V'<<24) + ('P'<<16) + ('A'<<8) + 'D',
-  xf_value_LAST = 100000  /* not a type */
-};
+/* Object: the common part of all objects */
+typedef struct xf_object_s {
+  uint32_t typecode;
+  /* ... will have further bytes ... */
+} *xf_object;
 
+/* Atom: a single scalar value */
 typedef struct xf_atom_s {
   uint32_t typecode;
   uint32_t value;
 } *xf_atom;
 
+/* Vector: a sequence of references to other objects */
 typedef struct xf_vect_s {
   uint32_t typecode;
-  uint32_t size;  /* overall, in bytes */
-  uint8_t bytes[1];
+  uint32_t level;
+  void *refs[1000];
 } *xf_vect;
+
+/* Pad: we must be able to create a pad of any valid length.
+ * We must also be able to turn any client object into a pad.
+ */
+typedef struct xf_pad_s {
+  uint32_t typecode;
+  uint32_t size;  /* overall length of pad, in bytes */
+  /* ... 0 or more additional bytes ... */
+} *xf_pad;
+
+/* Fwd: we must be able to turn any client object into a fwd.
+ * We need two types: fwdShort or fwdLong.
+ */
+typedef struct xf_fwdShort_s {
+  uint32_t typecode;
+  mps_addr_t fwd_pointer;
+} *xf_fwdShort;
+typedef struct xf_fwdLong_s {
+  uint32_t typecode;
+  mps_addr_t fwd_pointer;
+  uint32_t size;  /* overall length of fwd, in bytes */
+  /* ... 0 or more additional bytes ... */
+} *xf_fwdLong;
 
 #define XF_ALIGN 8
 mps_align_t XF_align = XF_ALIGN;
 
 static mps_res_t XF_scan(mps_ss_t ss, mps_addr_t a1, mps_addr_t a2)
 {
-  return MPS_RES_OK;
+  nyi("XF_scan");
 }
 
 static mps_addr_t XF_skip(mps_addr_t a1)
 {
-  return (char *)a1 + XF_align;
+  xf_object obj = (xf_object)a1;
+  uint32_t size = 0;
+  
+  switch(obj->typecode) {
+    case xf_typecode_atom: {
+      size = sizeof(struct xf_atom_s);
+      break;
+    }
+    case xf_typecode_vect: {
+      size = sizeof(struct xf_vect_s);
+      break;
+    }
+    case xf_typecode_pad: {
+      xf_pad pad = (xf_pad)a1;
+      size = pad->size;
+      break;
+    }
+    case xf_typecode_fwdShort: {
+      size = sizeof(struct xf_fwdShort_s);
+      break;
+    }
+    case xf_typecode_fwdLong: {
+      xf_fwdLong fwdLong = (xf_fwdLong)a1;
+      size = fwdLong->size;
+      break;
+    }
+    default: {
+      error("XF_skip: unrecognised typecode");
+      break;
+    }
+  }
+  return (char *)a1 + size;
 }
 
 static void XF_copy(mps_addr_t a1, mps_addr_t a2)
@@ -90,18 +146,10 @@ static mps_addr_t XF_isfwd(mps_addr_t a1)
 
 static void XF_pad(mps_addr_t a1, size_t size)
 {
-  if(size == 8) {
-    xf_atom a = (xf_atom)a1;
-    a->typecode = xf_type_apad;
-    a->value = xf_value_apad;
-  } else {
-    xf_vect v = (xf_vect)a1;
-    if(size <= 9) {
-      error("XF_pad: wrong size to pad (too small)");
-    }
-    v->typecode = xf_type_vpad;
-    v->size = size;
-  }
+  xf_pad p = (xf_pad)a1;
+  
+  p->typecode = xf_typecode_pad;
+  p->size = size;
 }
 
 static mps_addr_t XF_mps_class(mps_addr_t a1)
@@ -127,13 +175,12 @@ static xf_atom make_atom(mps_ap_t ap)
   size_t cb = sizeof(struct xf_atom_s);
   do {
     res = mps_reserve(&p, ap, cb);
-    outres("mps_reserve");
     if(res != MPS_RES_OK)
       goto fail_Reserve;
     neo = p;
   
     /* initialize new object */
-    neo->typecode = 0;
+    neo->typecode = xf_typecode_atom;
     neo->value = 0;
   } while(!mps_commit(ap, p, cb));
   
@@ -145,6 +192,11 @@ fail_Reserve:
   return NULL;
 }
 
+static show_usage(mps_arena_t arena)
+{
+  size_t size = mps_arena_committed(arena);
+  printf("Usage: committed memory: %X.\n", size);
+}
 
 struct mps_gen_param_s gens[] = {
   /* Generation 0 */
@@ -169,29 +221,41 @@ int main(int argc, char **argv)
   /* arena */
   pBlock = malloc(cbBlock);
   res = mps_arena_create(&arena, mps_arena_class_cl(), cbBlock, pBlock);
-  outres("mps_arena_create");
+  checkres("mps_arena_create");
   
   /* declare stack and regs as ambiguous roots */
   
   /* format */
   res = mps_fmt_create_B(&fmt, arena, &fmt_B_parts);
-  outres("mps_fmt_create_B");
+  checkres("mps_fmt_create_B");
   
   /* chain */
   res = mps_chain_create(&chain, arena, GEN_COUNT, gens);
-  outres("mps_chain_create");
+  checkres("mps_chain_create");
   
   /* pool */
   res = mps_pool_create(&pool, arena, mps_class_amc(), fmt, chain);
-  outres("mps_pool_create");
+  checkres("mps_pool_create");
   
   /* ap */
   res = mps_ap_create(&ap, pool);
-  outres("mps_ap_create");
+  checkres("mps_ap_create");
 
   /* make some stuff */
-  atom = make_atom(ap);
-  printf("Created atom at address: %p.\n", (void*)atom);
+  show_usage(arena);
+  {
+    int i;
+    for(i = 0; i < 1000; i += 1) {
+      atom = make_atom(ap);
+      atom->value = i;
+    }
+    printf("Created atom at address: %p.\n", (void*)atom);
+  }
+  show_usage(arena);
+
+  /* try a collection! */
+  res = mps_arena_collect(arena);
+  checkres("mps_arena_collect");
 
   /* tidy */
   mps_ap_destroy(ap);
@@ -201,6 +265,5 @@ int main(int argc, char **argv)
   mps_arena_destroy(arena);
     
   free(pBlock);
-  printf("hello, world!\n");
   return 0;
 }
