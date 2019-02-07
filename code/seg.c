9c9680be6675846e802e997764bb50278edc6efa
Alistair Turnbull <apt@ravenbrook.com>
2019-02-07 13:14:06 +0000

    Access ZoneSet using the RefSetZones abstraction.
    Copied from Perforce
     Change: 195994

--- code/seg.c
+++ code/seg.c
@@ -1513,7 +1513,7 @@ static void gcSegFinish(Inst inst)
   }
 
   EVENT5(SegSetSummary, PoolArena(SegPool(seg)), seg, SegSize(seg),
-         gcseg->summary.zones, RefSetEmpty->zones);
+         RefSetZones(&gcseg->summary), ZoneSetEMPTY);
 
   RefSetInit(&gcseg->summary);
 
@@ -1766,7 +1766,7 @@ static void gcSegSetSummary(Seg seg, RefSet summary)
   AVER_CRITICAL(&gcseg->segStruct == seg);
 
   EVENT5(SegSetSummary, PoolArena(SegPool(seg)), seg, SegSize(seg),
-         gcseg->summary.zones, summary->zones);
+         RefSetZones(&gcseg->summary), RefSetZones(summary));
 
   RefSetCopy(&gcseg->summary, summary);
 
@@ -1807,7 +1807,7 @@ static void gcSegSetRankSummary(Seg seg, RankSet rankSet, RefSet summary)
   seg->rankSet = BS_BITFIELD(Rank, rankSet);
 
   EVENT5(SegSetSummary, PoolArena(SegPool(seg)), seg, SegSize(seg),
-         gcseg->summary.zones, summary->zones);
+         RefSetZones(&gcseg->summary), RefSetZones(summary));
 
   RefSetCopy(&gcseg->summary, summary);
 }
