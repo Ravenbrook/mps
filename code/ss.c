d25d7121e56c00feb3c4c71f313bb3af4f3c4717
Gareth Rees <gdr@ravenbrook.com>
2019-10-10 15:18:03 +0100

    Avoid warning from gcc 5.4.0.
    Copied from Perforce
     Change: 196419
    
    (cherry picked from commit 4ea92e6d99e22157d528949cd41ceec07df17e89)

--- code/ss.c
+++ code/ss.c
@@ -1,7 +1,7 @@
 /* ss.c: STACK SCANNING
  *
  * $Id$
- * Copyright (c) 2001-2014 Ravenbrook Limited.  See end of file for license.
+ * Copyright (c) 2001-2019 Ravenbrook Limited.  See end of file for license.
  *
  * This scans the mutator's stack and fixes the registers that may
  * contain roots. See <design/stack-scan/>.
@@ -46,7 +46,9 @@ Res StackScan(ScanState ss, void *stackCold,
 {
   StackContextStruct scStruct;
   Arena arena;
-  void *warmest;
+  /* Avoid the error "variable might be clobbered by 'longjmp'" from
+     GCC by specifying volatile. See job004113. */
+  void * volatile warmest;
 
   AVERT(ScanState, ss);
 
@@ -55,10 +57,10 @@ Res StackScan(ScanState ss, void *stackCold,
   AVER(arena->stackWarm != NULL);
   warmest = arena->stackWarm;
   if (warmest == NULL) {
-    /* Somehow missed saving the context at the entry point (see
-       <design/stack-scan/#sol.entry-points.fragile>): do it now. */
-    STACK_CONTEXT_SAVE(&scStruct);
+    /* Somehow missed saving the context at the entry point
+       <design/stack-scan#.sol.entry-points.fragile>: do it now. */
     warmest = &scStruct;
+    STACK_CONTEXT_SAVE(&scStruct);
   }
 
   AVER(warmest < stackCold);                            /* .assume.desc */
@@ -69,7 +71,7 @@ Res StackScan(ScanState ss, void *stackCold,
 
 /* C. COPYRIGHT AND LICENSE
  *
- * Copyright (C) 2001-2014 Ravenbrook Limited <http://www.ravenbrook.com/>.
+ * Copyright (C) 2001-2019 Ravenbrook Limited <http://www.ravenbrook.com/>.
  * All rights reserved.  This is an open source license.  Contact
  * Ravenbrook for commercial licensing options.
  * 
